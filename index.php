d5c7870315b66cb7cc58443ddd5c2c4f1b08212d
Robbie Lopez <rolopez.email@gmail.com>
2017-04-12 09:37:51 -0600

    added table and lists

--- index.php
+++ index.php
@@ -12,6 +12,61 @@
 		<title>HTML SANDBOX</title>
 	</head>
 	<body>
-		<h1>Yo! This is an HTML page!</h1>
+
+		<header>
+			<h1>HTML SANDBOX</h1>
+		</header>
+		<main>
+			<h2>Commmander Riker Says:</h2>
+			<p>About four years. I got tired of hearing how young I
+			<p>More words to have another paragraph</p>
+
+			<img src="images/startrekcats.jpg" alt="star trek cat" />
+
+			<p>This is a link to Robbie's favorite site: <a href="http://www.espn.com" target="_blank">ESPN</a> </p>
+
+			<p>This is a link to an internal page: <a href="about/about.php">About Page</a> </p>
+
+			<h2>A few of my favorite things:</h2>
+			<ul>
+				<li>Tacos</li>
+				<li>dogs</li>
+				<li>acton movies</li>
+				<li>someone</li>
+			</ul>
+
+			<h2>A few of sebatsians favorite things:</h2>
+			<ol>
+				<li>spiderman</li>
+				<li>albuqueruqe</li>
+				<li>trolls</li>
+			</ol>
+
+			<h2>Cats &amp; Dogs</h2>
+			<table>
+				<tr>
+					<th>Cats</th>
+					<th>Dogs</th>
+				</tr>
+				<tr>
+					<td>Tabby</td>
+					<td>Bulldog</td>
+				</tr>
+				<tr>
+					<td>Persian</td>
+					<td>Beagle</td>
+				</tr>
+				<tr>
+					<td>Senator</td>
+					<td>Janitor</td>
+				</tr>
+				<tr>
+					<td>Grumpy</td>
+					<td>Happy</td>
+				</tr>
+			</table>
+
+		</main>
+
 	</body>
 </html>
